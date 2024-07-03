cb866063f2eee5f681e113a6b3dcfe11263c85f2
Guilherme WIlliam <guolhermewillamgarcia@gmail.com>
2024-07-02 21:09:18 -0300

    feito do 1 até 6

---
+++ e.js
@@ -0,0 +1,21 @@
+//let nome = "João"
+//let b = 10
+//let a = 5
+//let primeironome = "Josefa"
+//let segundonome = "Silva"
+
+//let frutas = [ "banana", "laranja" ]
+//frutas [4]
+//frutas.push("abacate")
+//console.log(frutas)
+//frutas.push("abacate")
+//console.log(frutas[0], frutas[frutas.lenght-1])
+
+let cores = ["vermelho", "azul", "amarelo"]
+console.log(cores[2]);
+console.log(cores[0])
+
+for(let i = 0; i < 2; i++){
+     ChannelMergerNode(vetor[i])
+}
+   
